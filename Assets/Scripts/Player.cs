d71a2f0c707cf24d623d6d443b17b41c313ec886
Randy Powelson <randy.powelson@yahoo.com>
2024-05-11 17:51:38 -0700

    Added jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpHeight;
     private float horizontal;
+    private Rigidbody playerRb;
+
+    private void Start(){
+        playerRb = GetComponent<Rigidbody>();
+    }
+
 
 
     private void Update(){
         horizontal = Input.GetAxisRaw("Horizontal");
 
         transform.position += new Vector3(horizontal * movementSpeed * Time.deltaTime, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            playerRb.AddForce(new Vector3(0, jumpHeight, 0));
+        }
     }
 }
